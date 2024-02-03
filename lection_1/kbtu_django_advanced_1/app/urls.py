274f436aedb2570460167337f007a9857b039450
Julia <98633575+tsoyjulia0@users.noreply.github.com>
2024-02-03 13:03:37 +0600

    commit

--- lection_1/kbtu_django_advanced_1/app/urls.py
+++ lection_1/kbtu_django_advanced_1/app/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 
-from app.views import index, add_student
+from app1.views import index, add_student, update_student, delete_student
 
 urlpatterns = [
     path("", index, name='index'),
-    path("add", add_student, name='add'),
+    path("add/", add_student, name='add'),
+    path('update/<int:index>/', update_student, name='update_student'),
+    path('delete/<int:index>/', delete_student, name='delete_student'),
+
 ]
