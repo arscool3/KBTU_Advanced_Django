f9c4bab8d4907310e1efedd89b8d04af59ac5117
Damir <mrdnigel@gmail.com>
2024-02-03 17:40:19 +0600

    Revert "FastApi films practice task"
    This reverts commit 9ab0a2cb471a0bd6456fc3e7a16b6663d463e893.

--- main.py
+++ main.py
@@ -1,60 +1,16 @@
-from typing import List, Annotated
+# This is a sample Python script.
 
-from fastapi import FastAPI, Query
-from pydantic import BaseModel, Field
-
-app = FastAPI()
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-class Film(BaseModel):
-    name: str = Field(max_length=50)
-    description: str = Field(max_length=300)
-    director: str = Field(max_length=50)
-    rating: int = Field(gt=0, le=100)
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
 
-films = []
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
 
-_film_rating = Annotated[int, Query(gt=0, le=100)]
-
-@app.post('/film/')
-def add_film(film: Film) -> str:
-    films.append(film)
-    return "Film added successfully"
-
-
-@app.get('/film_by_rating')
-def get_film_by_rating(rating: _film_rating) -> list[Film]:
-    return [film for film in films if film.rating == rating]
-
-
-@app.get('/films/')
-def get_films(limit: int, page: int = Query(gt=0)) -> list[Film]:
-    return films[limit * (page-1): min(len(films), limit * page)]
-
-
-@app.get('/film/{id}')
-def test(id: int) -> str:
-    return f"Film with id {id} - {films[id]}"
-
-# uvicorn main:app --reload
-
-
-def a(a:int):
-    def decorator(func):
-        from datetime import datetime
-
-        def wrapped(*args, ):
-            print("Started", datetime.now(), a)
-            func()
-            print("Ended", datetime.now())
-
-        return wrapped
-    return decorator
-
-# @a(a=10)
-# def x():
-#     for _ in range(1_000_000_000):
-#         continue
-#
-# x()
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
