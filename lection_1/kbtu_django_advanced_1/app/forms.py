fefe08690cf93ffba25b8a2da1b412f7852a63f4
Julia <98633575+tsoyjulia0@users.noreply.github.com>
2024-02-03 13:03:55 +0600

    commit

--- lection_1/kbtu_django_advanced_1/app/forms.py
+++ lection_1/kbtu_django_advanced_1/app/forms.py
@@ -3,3 +3,5 @@ from django import forms
 
 class Student(forms.Form):
     name = forms.CharField(max_length=50, label='name')
+    age = forms.IntegerField()
+    sex = forms.CharField(max_length=50, label='sex')
