96cd3cb5e93ed66594e6c0e55bcaf54d8a35c684
Aruzhan Orynbay <98757036+orynbay21@users.noreply.github.com>
2024-02-17 17:26:55 +0600

    homework1

--- main.py
+++ main.py
@@ -1,16 +1,40 @@
-# This is a sample Python script.
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel
+from typing import List, Optional
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
 
+class movie(BaseModel):
+    id: int
+    name: str
+    rating: float
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+movies = [
+    movie(id=1, name="little women", rating=8.5),
+    movie(id=2, name="emma", rating=9.9),
+    movie(id=3, name="pride and prejudice", rating=8.8),
+]
 
+@app.get("/movies", response_model=List[movie])
+def list_movies():
+    return movies
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+@app.get("/movies/{movie_id}", response_model=movie)
+def get_movie_by_id(movie_id: int):
+    for movie in movies:
+        if movie.id == movie_id:
+            return movie
+    raise HTTPException(status_code=404, detail="movie not found")
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.post("/movies", response_model=movie)
+def add_movie(movie: movie):
+    movies.append(movie)
+    return movie
+
+@app.get("/movies/by_rating")
+def get_movies_by_rating(rating: Optional[float] = None):
+    if rating is None:
+        return {"message": "what rating"}
+    filtered_movies = [movie for movie in movies if movie.rating >= rating]
+    return filtered_movies
+
