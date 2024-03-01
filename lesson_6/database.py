8fc14c06e701d3c89de3d9a5779bdce1c903f5aa
Arystanbek Saule <98650996+SamSweet04@users.noreply.github.com>
2024-03-02 00:37:27 +0600

    Delete lesson_6 directory

--- lesson_6/database.py
+++
@@ -1,7 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import Session
-
-url = 'postgresql://postgres:Haker15987@localhost/postgres3'
-
-engine = create_engine(url)
-session = Session(engine)
