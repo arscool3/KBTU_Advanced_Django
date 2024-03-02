f04afc0dc139afa1005990e2b02cc6c7b41a35b9
qorganbek <dimashqorganbek@gmail.com>
2024-03-02 17:44:49 +0500

    test

---
+++ practice7/models.py
@@ -0,0 +1,29 @@
+from typing import Annotated
+import sqlalchemy
+from sqlalchemy.orm import mapped_column, Mapped, relationship
+from database import Base
+
+_id = Annotated[int, mapped_column(sqlalchemy.Integer, primary_key=True)]
+
+
+class Cinema:
+    id: Mapped[_id]
+    name: Mapped[str]
+
+
+class Genre(Cinema, Base):
+    __tablename__ = 'genres'
+    film: Mapped['Film'] = relationship(back_populates='genres')
+
+
+class Director(Cinema, Base):
+    __tablename__ = 'directors'
+    film: Mapped['Film'] = relationship(back_populates='directors')
+
+
+class Film(Cinema, Base):
+    __tablename__ = 'films'
+    director_id: Mapped[int] = mapped_column(sqlalchemy.ForeignKey('directors.id'))
+    genre_id: Mapped[int] = mapped_column(sqlalchemy.ForeignKey('genres.id'))
+    directors: Mapped[list[Director]] = relationship(back_populates='film')
+    genres: Mapped[list[Genre]] = relationship(back_populates='film')
