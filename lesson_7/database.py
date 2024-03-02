f973aecd65e41d1864900615a08c3b71d4a38e97
Aitolkyn <aitolkynamantai17@gmail.com>
2024-03-02 23:54:27 +0600

    done

--- lesson_7/database.py
+++ lesson_7/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
 
-url = 'postgresql://postgres:601246@localhost:5432/lesson7'
+url = 'postgresql://postgres:postgres6@localhost:5432/lesson7'
 engine = create_engine(url)
 session = Session(engine)
 
