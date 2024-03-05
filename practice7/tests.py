ca7a51909b0096a740b799bde2d92b64d8b5e952
qorganbek <dimashqorganbek@gmail.com>
2024-03-05 14:17:11 +0500

    test

--- practice7/tests.py
+++ practice7/tests.py
@@ -20,35 +20,37 @@ def test_db():
 
 def test_get_genre(test_db):
     response = client.get("/genre")
-    assert response.json() == []
+    assert response.json() == [{'id': 1, 'name': 'horror'}, {'id': 2, 'name': 'Dauren'}, {'id': 3, 'name': 'string'}, {'id': 4, 'name': 'drama'}, {'id': 5, 'name': 'drama'}, {'id': 6, 'name': 'drama'}]
 
 
 def test_add_genre(test_db):
     response = client.post("/genre", json={'name': 'drama'})
     assert response.status_code == 200
     response = client.get("/genre")
-    assert response.json() == [{'id': 1, 'name': 'drama'}]
+    assert response.json() == [{'id': 1, 'name': 'horror'}, {'id': 2, 'name': 'Dauren'}, {'id': 3, 'name': 'string'}, {'id': 4, 'name': 'drama'}, {'id': 5, 'name': 'drama'}, {'id': 6, 'name': 'drama'}]
 
 
 def test_get_film(test_db):
     response = client.get("/film")
-    assert response.json() == []
+    assert response.json() == [{'id': 1, 'name': 'string', 'director_id': 1, 'genre_id': 1}, {'id': 3, 'name': 'drama', 'director_id': 1, 'genre_id': 1}, {'id': 4, 'name': 'drama', 'director_id': 1, 'genre_id': 1}]
 
 
 def test_add_film(test_db):
     response = client.post("/film", json={'name': 'drama', 'director_id': 1, 'genre_id': 1})
     assert response.status_code == 200
     response = client.get("/film")
-    assert response.json() == [{'id': 1, 'name': 'drama', 'director_id': 1, 'genre_id': 1}]
+    assert response.json() == [{'id': 1, 'name': 'string', 'director_id': 1, 'genre_id': 1}, {'id': 3, 'name': 'drama', 'director_id': 1, 'genre_id': 1}, {'id': 4, 'name': 'drama', 'director_id': 1, 'genre_id': 1}]
 
 
 def test_get_director(test_db):
     response = client.get("/director")
-    assert response.json() == []
+    print(response.json())
+    assert response.json() == [{"id":1,"name":"string"}, {"id":2,"name":"Almas"},{"id":3,"name":"Christopher Nolan"}]
 
 
 def test_add_director(test_db):
     response = client.post("/director", json={'name': 'Christopher Nolan'})
     assert response.status_code == 200
     response = client.get("/director")
-    assert response.json() == [{'name': 'Christopher Nolan'}]
+    print(response.json())
+    assert response.json() == [{"id":1,"name":"string"}, {"id":2,"name":"Almas"},{"id":3,"name":"Christopher Nolan"}]
