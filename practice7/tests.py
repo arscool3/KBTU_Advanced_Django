1c6011c5bc305bd40092d3f71f22d47e6f60129f
qorganbek <dimashqorganbek@gmail.com>
2024-03-05 13:54:40 +0500

    test

--- practice7/tests.py
+++ practice7/tests.py
@@ -6,7 +6,7 @@ from main import app
 from database import Base, engine
 
 client = TestClient(app)
-url = 'postgresql://postgres:postgres@localhost:5433/postgres'
+url = 'postgresql://postgres:Dimash2003@localhost:5432/test_adv_django'
 engine = sqlalchemy.create_engine(url)
 session = Session(engine)
 
@@ -28,3 +28,27 @@ def test_add_genre(test_db):
     assert response.status_code == 200
     response = client.get("/genre")
     assert response.json() == [{'id': 1, 'name': 'drama'}]
+
+
+def test_get_film(test_db):
+    response = client.get("/film")
+    assert response.json() == []
+
+
+def test_add_film(test_db):
+    response = client.post("/film", json={'name': 'drama', 'director_id': 1, 'genre_id': 1})
+    assert response.status_code == 200
+    response = client.get("/film")
+    assert response.json() == [{'id': 1, 'name': 'drama', 'director_id': 1, 'genre_id': 1}]
+
+
+def test_get_director(test_db):
+    response = client.get("/director")
+    assert response.json() == []
+
+
+def test_add_director(test_db):
+    response = client.post("/director", json={'name': 'Christopher Nolan'})
+    assert response.status_code == 200
+    response = client.get("/director")
+    assert response.json() == [{'name': 'Christopher Nolan'}]
