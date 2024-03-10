b168d8c11d3ddc6448a273492ddf31c267f47413
Danila Khlebokazov <danilik_2003@mail.ru>
2024-03-10 15:39:16 +0500

    :sparkles: api for fines and insurance finished

--- midterm/schemas/create_schemas.py
+++ midterm/schemas/create_schemas.py
@@ -15,3 +15,17 @@ class CreateVehicle(BaseModel):
     mark_name: str
     name: str
     vin: str
+
+
+class CreateLocation(BaseModel):
+    latitude: float
+    longitude: float
+
+
+class CreateFine(BaseModel):
+    fine_amount: int
+    location: CreateLocation
+
+
+class CreateInsurance(BaseModel):
+    insurance_amount: int
--- midterm/schemas/model_schemas.py
+++ midterm/schemas/model_schemas.py
@@ -13,13 +13,35 @@ class Mark(BaseClass):
     name: str
 
 
+class OnlyVehicle(BaseClass):
+    mark: Mark
+    name: str
+    vin: str
+
+
+class OnlyLocation(BaseClass):
+    latitude: float
+    longitude: float
+
+
+class OnlyFine(BaseClass):
+    fined_vehicle: OnlyVehicle
+    fine_amount: int
+    location: OnlyLocation
+
+
+class PersonInsurance(BaseClass):
+    insurance_amount: int
+    vehicles: list[OnlyVehicle]
+
+
 class Person(BaseModel):
     iin: str
     name: str
     surname: str
-    fines: list = []
-    vehicles: list = []
-    insurances: list = []
+    fines: list[OnlyFine] = []
+    vehicles: list[OnlyVehicle] = []
+    insurances: list[PersonInsurance] = []
 
     class Config:
         from_attributes = True
@@ -34,10 +56,21 @@ class OnlyPerson(BaseModel):
         from_attributes = True
 
 
+class VehicleFine(BaseClass):
+    fined_person: OnlyPerson
+    fine_amount: int
+    location: OnlyLocation
+
+
+class VehicleInsurance(BaseClass):
+    insurance_amount: int
+    people: list[OnlyPerson]
+
+
 class Vehicle(BaseClass):
     mark: Mark
     name: str
     vin: str
     owner: OnlyPerson
-    fines: list = []
-    insurances: list = []
+    fines: list[VehicleFine] = []
+    insurances: list[VehicleInsurance] = []
