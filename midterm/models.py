1f4650a3368ff9b545932b046dc4b8f68269209b
murap <murapov.yerkhan@gmail.com>
2024-03-15 21:04:52 +0500

    midterm

---
+++ midterm/models.py
@@ -0,0 +1,78 @@
+import uuid
+from sqlalchemy import ForeignKey, Table, Column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+from database import Base
+
+associations_post_favorite = Table(
+    'associations_post_favorite',
+    Base.metadata,
+    Column("post_id", ForeignKey("posts.id"), primary_key=True),
+    Column("favorite_id", ForeignKey("favorites.id"), primary_key=True),
+)
+
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id: Mapped[str] = mapped_column(primary_key=True, default=uuid.uuid4)
+    username: Mapped[str] = mapped_column(unique=True, nullable=False)
+    first_name: Mapped[str]
+    last_name: Mapped[str]
+    password: Mapped[str] = mapped_column(nullable=False)
+    posts: Mapped[list["Post"]] = relationship("Post", back_populates="author", cascade="all, delete-orphan")
+    favorite: Mapped["Favorite"] = relationship("Favorite", back_populates="owner", uselist=False)
+    comments: Mapped[list["Comment"]] = relationship("Comment", back_populates="owner", cascade="all, delete-orphan")
+    like: Mapped[list["Like"]] = relationship("Like", back_populates="owner", cascade="all, delete-orphan")
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id: Mapped[str] = mapped_column(primary_key=True, default=uuid.uuid4)
+    title: Mapped[str] = mapped_column(nullable=False)
+    content: Mapped[str] = mapped_column(nullable=False)
+    author_id: Mapped[str] = mapped_column(ForeignKey("users.id"))
+    author: Mapped["User"] = relationship("User", back_populates="posts")
+    favorites: Mapped[list["Favorite"]] = relationship(secondary=associations_post_favorite, back_populates="posts")
+    category_id: Mapped[str] = mapped_column(ForeignKey("categories.id"))
+    category: Mapped["Category"] = relationship("Category", back_populates="posts")
+    comments: Mapped[list["Comment"]] = relationship("Comment", back_populates="post", cascade="all, delete-orphan")
+    like: Mapped[list["Like"]] = relationship("Like", back_populates="post", cascade="all, delete-orphan")
+
+
+class Favorite(Base):
+    __tablename__ = 'favorites'
+
+    id: Mapped[str] = mapped_column(primary_key=True, default=uuid.uuid4)
+    owner_id: Mapped[str] = mapped_column(ForeignKey("users.id"))
+    owner: Mapped["User"] = relationship("User", back_populates="favorite", uselist=False)
+    posts: Mapped[list["Post"]] = relationship(secondary=associations_post_favorite, back_populates="favorites")
+
+
+class Category(Base):
+    __tablename__ = 'categories'
+
+    id: Mapped[str] = mapped_column(primary_key=True, default=uuid.uuid4)
+    name: Mapped[str] = mapped_column(unique=True, nullable=False)
+    posts: Mapped[list["Post"]] = relationship("Post", back_populates="category")
+
+
+class Comment(Base):
+    __tablename__ = 'comments'
+
+    id: Mapped[str] = mapped_column(primary_key=True, default=uuid.uuid4)
+    body: Mapped[str] = mapped_column(nullable=False)
+    owner_id: Mapped[str] = mapped_column(ForeignKey("users.id"))
+    owner: Mapped["User"] = relationship("User", back_populates="comments")
+    post_id: Mapped[str] = mapped_column(ForeignKey("posts.id"))
+    post: Mapped["Post"] = relationship("Post", back_populates="comments")
+
+
+class Like(Base):
+    __tablename__ = 'like'
+
+    id: Mapped[str] = mapped_column(primary_key=True, default=uuid.uuid4)
+    owner_id: Mapped[str] = mapped_column(ForeignKey("users.id"))
+    owner: Mapped["User"] = relationship("User", back_populates="like")
+    post_id: Mapped[str] = mapped_column(ForeignKey("posts.id"))
+    post: Mapped["Post"] = relationship("Post", back_populates="like")
