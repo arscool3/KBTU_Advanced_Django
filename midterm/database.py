cd50b8f996cff892c2db69404b1985cacd042cda
Damirrrrr <amanzholovdamir@inbox.ru>
2024-03-16 15:30:32 +0500

    backup midka

--- midterm/database.py
+++ midterm/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import Mapped
 from sqlalchemy.ext.declarative import declarative_base
 
-url = 'postgresql://postgresql:postgresql@postgresql/postgresql'
+url = 'postgresql://postgres:postgres1@postgres/postgres'
 
 engine = create_engine(url)
 
--- midterm/main.py
+++ midterm/main.py
@@ -1,8 +1,12 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from typing import Annotated
-from models import Team, Player
+from models import Team, Player, League
+
 app = FastAPI()
+
+
+
 
 @app.get('/teams')
 def get_teams() -> list[Team]:
@@ -14,4 +18,19 @@ def get_team_by_id() -> Team:
 
 @app.get('/teams/{id}/players')
 def get_players_of_team() -> list[Player]:
+    pass
+
+
+@app.post('/teams')
+def add_team(team: Team) -> str:
+    pass
+
+
+@app.post('/player')
+def add_player(player: Player) -> str:
+    pass
+
+
+@app.post('/league')
+def add_league(league: League) -> str:
     pass
