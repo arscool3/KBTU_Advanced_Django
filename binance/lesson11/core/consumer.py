c9fd8afd4a4a8b10d9a760152ed803b177dc8258
Julia <98633575+tsoyjulia0@users.noreply.github.com>
2024-04-13 19:38:11 +0500

    updated version

--- binance/lesson11/core/consumer.py
+++ binance/lesson11/core/consumer.py
@@ -5,9 +5,9 @@ import confluent_kafka
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
-from lesson11 import database
-from lesson11.core.schemas import CreateData, Binance
-from lesson11.main import get_db
+from database import Data
+from schemas import CreateData, Binance
+from database import get_db
 
 consumer = confluent_kafka.Consumer(
     {"bootstrap.servers": "localhost:9092", "group.id": "main_group"}
@@ -31,18 +31,32 @@ def consume(db: Session = Depends(get_db)):
 
             for message in messages:
                 binance = Binance.model_validate(json.loads(message.value().decode("utf-8")))
+                send_coin_amount = binance.pair.send_coin.amount
+                get_coin_amount = binance.pair.get_coin.amount
+
+                if send_coin_amount is not None and get_coin_amount is not None and get_coin_amount != 0:
+                    k_to_usd = send_coin_amount - get_coin_amount / get_coin_amount
+                else:
+                    k_to_usd = 0.0
+
+                db = next(get_db())
                 tradeData = CreateData(
                     time=datetime.now(),
-                    name=f'{binance.pair.send_coin}_to_{binance.pair.get_coin}',
-                    k_to_usd=binance.pair.send_coin.amount / binance.pair.get_coin.amount
+                    name=f'{binance.pair.send_coin.cur_name}_to_{binance.pair.get_coin.cur_name}',
+                    k_to_usd=k_to_usd
                 )
-                db.add(database.Data(**tradeData.model_dump()))
+                db.add(Data(**tradeData.model_dump()))
                 print("Data has been inserted")
+                db.commit()
     except Exception as e:
         print(f"Raised {e}")
     finally:
-        consumer.close()
+        db.close()
+        # consumer.close()
 
 
 if __name__ == '__main__':
-    consume()
+    try:
+        consume()
+    finally:
+        consumer.close()
