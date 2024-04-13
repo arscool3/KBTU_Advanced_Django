71bd0bde4056f901131c4966d4f2b62949399943
Julia <98633575+tsoyjulia0@users.noreply.github.com>
2024-04-13 19:39:46 +0500

    updated version

--- binance/lesson11/database.py
+++ binance/lesson11/database.py
@@ -1,22 +1,30 @@
-from fastapi import FastAPI
-from database import session
-from consumer import consume
+from sqlalchemy import create_engine, Column, func, DateTime
+from sqlalchemy.orm import declarative_base, Session, mapped_column, Mapped
+from sqlalchemy.orm import sessionmaker
 
-app = FastAPI()
-
-
-@app.get("/")
-def root():
-    return "OK"
+url = 'postgresql://postgres:postgres@localhost:5439/postgres'
+engine = create_engine(url)
+session = Session(engine)
+Base = declarative_base()
 
 
-@app.get("/heath_check")
-def health_check() -> str:
-    return "I'm alive"
+def get_db():
+    try:
+        yield session
+        session.commit()
+    except Exception:
+        raise
+    finally:
+        session.close()
 
 
-if __name__ == "__main__":
-    consume()
+# _id = Annotated[int, mapped_column(sqlalchemy.Integer, primary_key=True)]
+class Data(Base):
+    __tablename__ = 'data'
 
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    time: Mapped[str] = Column(DateTime, default=func.utcnow)
+    name: Mapped[str] = mapped_column(nullable=False)
+    k_to_usd: Mapped[float] = mapped_column(nullable=False)
 
-# Address: localhost/8002
+# Docker: 1ed6fa6ef3ee8fb1bd91524b38857389b32ba397b01e4bf92476ac8f270eb42a
