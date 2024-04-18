3a64da8d290200d25a0fc830761b4731313db253
Aitolkyn <aitolkynamantai17@gmail.com>
2024-04-18 16:29:37 +0500

    binance

--- lesson_10-binance/schemas.py
+++ lesson_10-binance/schemas.py
@@ -1,12 +1,6 @@
 from pydantic import BaseModel
 
 
-# class Bitcoin(BaseModel):
-#     name: str
-#     price: float
-#     start_date: str
-#     end_date: str
-
 class BaseBitcoin(BaseModel):
     name: str
     price: float
--- lesson_10-binance/services.py
+++ lesson_10-binance/services.py
@@ -3,26 +3,6 @@ from datetime import datetime
 from schemas import Bitcoin
 from db import session
 
-
-# def fetch_binance_data(symbol, interval, start_str, end_str):
-#     url = 'https://api.binance.com/api/v3/klines'
-#     params = {
-#         'symbol': symbol,
-#         'interval': interval,
-#         'startTime': int(datetime.strptime(start_str, '%Y-%m-%d %H:%M:%S').timestamp() * 1000),
-#         'endTime': int(datetime.strptime(end_str, '%Y-%m-%d %H:%M:%S').timestamp() * 1000),
-#     }
-#     response = requests.get(url, params=params)
-#     data = response.json()
-#
-#     return [
-#         {
-#             'name': symbol,
-#             'price': candle[1],
-#             'start_date': datetime.fromtimestamp(candle[0] / 1000).strftime('%Y-%m-%d %H:%M:%S'),
-#             'end_date': datetime.fromtimestamp(candle[9] / 1000).strftime('%Y-%m-%d %H:%M:%S')
-#         } for candle in data
-#     ]
 def fetch_binance_data(symbol, interval, start_str, end_str):
     url = 'https://api.binance.com/api/v3/klines'
     params = {
@@ -36,13 +16,12 @@ def fetch_binance_data(symbol, interval, start_str, end_str):
 
     transformed_data = []
     for candle in data:
-        # Convert the timestamp and closing time to integer before performing the division
-        start_timestamp = int(candle[0]) // 1000  # Opening time is the first element
-        end_timestamp = int(candle[6]) // 1000  # Closing time is the seventh element
+        start_timestamp = int(candle[0]) // 1000
+        end_timestamp = int(candle[6]) // 1000
 
         transformed_data.append({
             'name': symbol,
-            'price': float(candle[1]),  # Open price is the second element, convert string to float
+            'price': float(candle[1]),
             'start_date': datetime.fromtimestamp(start_timestamp).strftime('%Y-%m-%d %H:%M:%S'),
             'end_date': datetime.fromtimestamp(end_timestamp).strftime('%Y-%m-%d %H:%M:%S')
         })
