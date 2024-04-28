43d24a8c2447c4de26816d95690ab227fb5b703c
Danial <bidaybek044@gmail.com>
2024-04-28 16:24:07 +0500

    final

--- FINAL_task-tracker/utils/model_constants.py
+++
@@ -1,6 +0,0 @@
-from typing import Annotated
-
-import sqlalchemy
-from sqlalchemy.orm import mapped_column
-
-default_id = Annotated[int, mapped_column(sqlalchemy.Integer, primary_key=True)]
--- FINAL_task-tracker/utils/schemas_config.py
+++
@@ -1,7 +0,0 @@
-from pydantic import BaseModel
-
-
-class BaseSchema(BaseModel):
-
-    class Config:
-        from_attributes = True
