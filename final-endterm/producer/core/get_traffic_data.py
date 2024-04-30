dd81e240683a39e26853dc449d107e5d3cd2a03f
Danila Khlebokazov <danilik_2003@mail.ru>
2024-05-01 00:56:18 +0500

    :white_check_mark: rush test passed

--- final-endterm/producer/core/get_traffic_data.py
+++ final-endterm/producer/core/get_traffic_data.py
@@ -6,12 +6,12 @@ __all__ = ["get_person_traffic_data"]
 
 def get_person_traffic_data() -> dict:
     return {
-        "person_id": random.randint(1, 100),
+        "person_id": random.randint(1, 1),
         "traffic_data": {
             "timestamp": time.time(),
             "location": {
-                "latitude": round(random.uniform(0, 100), 5),
-                "longitude": round(random.uniform(0, 100), 5),
+                "latitude": round(random.uniform(1, 30), 5),
+                "longitude": round(random.uniform(1, 40), 5),
             },
             "speed": random.randint(0, 100),
             "acceleration": round(random.uniform(0, 100), 3),
