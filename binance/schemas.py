c628256c02db37baa04e7431927941067c6a2e7c
mapofthemoon <spnvva.a@gmail.com>
2024-05-06 15:39:45 +0500

    --

---
+++ binance/schemas.py
@@ -0,0 +1,18 @@
+from pydantic import BaseModel
+from datetime import datetime
+
+
+class BaseBitcoin(BaseModel):
+    time: datetime
+    price: float
+    coin: str
+
+    class Config:
+        from_attributes = True
+
+
+class Bitcoin(BaseBitcoin):
+    id: int
+
+class BitcoinCreate(BaseBitcoin):
+    pass
