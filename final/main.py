680151c7e5477c4aaa801ad140106453acb2a9c2
ZhanserikKalmukhambet <zhanserik.kalmukhambet@gmail.com>
2024-05-08 23:57:09 +0500

    configured model routers. implemented profile router

--- final/main.py
+++ final/main.py
@@ -1,7 +1,10 @@
 from fastapi import FastAPI
+import routers
 
-import auth
+from routers import auth, profile
 
 app = FastAPI()
 
+
 app.include_router(auth.router)
+app.include_router(profile.router)
--- final/schemas.py
+++ final/schemas.py
@@ -10,3 +10,14 @@ class CreateUserRequest(BaseModel):
 class Token(BaseModel):
     access_token: str
     token_type: str
+
+
+class CreateProfileRequest(BaseModel):
+    education: str
+    experience: int
+    stack: str
+    current_workplace: str
+    coverletter: str
+    user_id: int
+
+
