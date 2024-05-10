519c323fc1d4834d1ff61f5b7e7d3faf6fbaabbf
qorganbek <dimashqorganbek@gmail.com>
2024-05-10 07:55:07 +0500

    restaurant dockerfile bug fixed

--- final/AuthenticationService/config.py
+++ final/AuthenticationService/config.py
@@ -6,8 +6,8 @@ load_dotenv()
 DB_URL = os.environ.get("DB_URL1")
 SECRET_KEY = os.environ.get("SECRET_KEY")
 GATEWAY_TIMEOUT = 59
-CUSTOMER_SERVICE_URL = 'http://customer_service:8000'
-COURIER_SERVICE_URL = 'http://localhost:8001'
-EMAIL_SERVICE_URL = 'http://email_service:8000'
-RESTAURANT_SERVICE_URL = 'http://restaurant_service:8001'
-NOTIFICATION_SERVICE_URL = 'http://notification_service:8000'
+CUSTOMER_SERVICE_URL = os.environ.get('CUSTOMER_SERVICE_URL')
+COURIER_SERVICE_URL = os.environ.get('COURIER_SERVICE_URL')
+EMAIL_SERVICE_URL = os.environ.get('EMAIL_SERVICE_URL')
+RESTAURANT_SERVICE_URL = os.environ.get('RESTAURANT_SERVICE_URL')
+NOTIFICATION_SERVICE_URL = os.environ.get('NOTIFICATION_SERVICE_URL')
