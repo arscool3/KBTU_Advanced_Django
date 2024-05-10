8501e58dde8e40eb44b8d642a70c6ea65431cdb6
Nursultan <yerken1656@gmail.com>
2024-05-10 20:07:11 +0500

    upd: final2.0

--- final/task.py
+++ final/task.py
@@ -2,7 +2,7 @@ import dramatiq
 from dramatiq.results.backends.redis import RedisBackend
 from dramatiq.brokers.redis import RedisBroker
 from dramatiq.results import Results
-from background_task import process_payment
+from background_task import process_payment_with_service
 
 result_backend = RedisBackend()
 broker = RedisBroker()
@@ -12,5 +12,11 @@ dramatiq.set_broker(broker)
 
 @dramatiq.actor(store_results=True)
 def async_process_payment(payment_id, amount):
-    result = process_payment(payment_id, amount)
-    print(f"Payment processed: {result}")
+
+    print(f"Initiating payment processing for ID {payment_id} and amount ${amount}.")
+    result = process_payment_with_service(payment_id, amount)
+
+    if result and result.get("status") == "success":
+        print(f"Payment processed successfully: {result}")
+    else:
+        print("Failed to process payment or payment declined.")
